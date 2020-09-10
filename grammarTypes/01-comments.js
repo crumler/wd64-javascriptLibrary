4bb4b945cd51d9fe2052a07d804f203c577a8c3f
Chris Rumler <cgrumler@gmail.com>
2020-09-10 15:16:37 -0400

    first push

---
+++ grammarTypes/01-comments.js
@@ -0,0 +1,9 @@
+// this is a single line comment
+
+// quick note = CTRL + /
+
+/* this
+is a
+multi line
+comment
+*/
---
+++ grammarTypes/02-declarations.js
@@ -0,0 +1,76 @@
+/* Talking about variables */
+
+console.log("Hello there...");
+
+//console.log is a tool used for de-bugging
+
+let a = 2;
+
+let b = 1;
+
+console.log(a + b);
+
+//RESTRICTIONS
+
+/*
+
+- a variable name must begin with a letter, underscore, or a dollar sign
+- numbers may follow the above characters, BUT cannot come first
+- JavaScript is case-sensitive
+- no spaces are allowed within variable names
+- camelCase is best practice for naming
+
+*/
+
+/*
+
+KEYWORDS:
+
+var, let, and const
+
+    - var: 'old' keyword that stands for 'variable'.  ** we won't be using this as often, but it is still a viable keyword to use
+    - let: 'new' keyword for 'variable'.  This was introduced with ES6 *newest version of ECMAScript, which is a standaization of JavaScript*
+    - const: also a 'new' keyword that declares and UNCHANGABLE, or CONSTANT, variable.
+
+*/
+
+// var variable = 'var variable';
+// let letVariable = 'let variable';
+
+// let function = 1;  *Cannot use reserved words within a variable name - (such as 'function')
+
+/*
+
+DECLARATIONS & INITIALIZATIONS
+
+*/
+
+// let x; //this is a declaration (on the left side)
+
+// let x = 10;  //the right side is the initialization
+
+//10 is our initialization
+
+// let y;
+
+// console.log('declaration:', y);
+
+// y = 10;
+// console.log('initialization:', y);
+
+//const
+
+// let today = 'Great!';
+// const efa = 'Wonderful!';
+
+// console.log(today, efa);
+
+// today = 'Awesome!';
+
+// console.log(today, efa);
+
+// efa = 'Superb!';
+
+// console.log(today, efa);
+
+//const allows us to make a variable that we don't want to change
